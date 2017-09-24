2451765ef3a9a54e2cc0430fad486ae69c067dff
Saamer Mansoor <i@saamer.me>
2017-09-24 08:41:01 -0600

    AJAX fund works but not returning correct value

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//https://codepen.io/saamerm/pen/dVOvXK?editors=1111 
 // x=500;
 // document.getElementById("jsval").innerHTML = x;
 // //Remove stuff above
@@ -16,20 +15,53 @@ $(function() {
   x = 500;
   addValue("#val", x, 500, 5);
   // showText("#msg", "Hello, World!", 0, 500); //Remove this line
-//   var query="https://raw.githubusercontent.com/saamerm/Unearthed-Hackathon-Denver/master/values.json";
-//   $.ajax({
-//     url: query,
-//     type: "GET",
-//     dataType: "json"
-//     success: function(data) {
-//         var value = data[0].value; // get data value from json
-//         $("#data").val(value);
-//     console.log("Success");
-//     }
-//          error: {console.log("Error");}
+  var query =
+    "https://raw.githubusercontent.com/saamerm/Unearthed-Hackathon-Denver/master/values.json";
+  var y = getData(query, "#data");
+  $(jsval).append(y - 1 + " ");
+var z=getValue();
+    $(api).append(z + " ");
 
-// });  
+
+  //   $.ajax({
+  //     url: query,
+  //     type: "GET",
+  //     dataType: "json"
+  //     success: function(data) {
+  //         var value = data[0].value; // get data value from json
+  //         $("#data").val(value);
+  //     console.log("Success");
+  //     }
+  //          error: {console.log("Error");}
+
+  // });
 });
+var getData = function(query, target) {
+  $(target).append(query + " ");
+  x = 500;
+  // var data="";
+  // var $;
+  // $.ajax({
+  //           type: "GET",
+  //           // for community pages, use whole community url including path, e.g. 
+  //           // https://logan.blitz01.t.force.com/customers/services/proxy.
+  //           url: query, 
+  //           contentType: 'application/json',
+  //           // cache: false,
+  //           success : function(response) {
+  //                         alert("result" + response);
+  //                         console.log("result" + response);
+  //                  },
+  //           error : function(response) {
+  //                         alert("Failed" + response);
+  //             console.log("result" + response);
+  //                  },                 
+  //           dataType: "json",          
+  //           };
+  // var parsedData = JSON.parse(data);
+  // return parsedData.value;
+  return x;
+};
 
 var addValue = function(target, message, interval, loops) {
   for (i = 0; i < loops; i++) {
@@ -42,4 +74,24 @@ var addValue = function(target, message, interval, loops) {
 };
 
 //https://raw.githubusercontent.com/saamerm/Unearthed-Hackathon-Denver/master/values.json
-
+function getValue() {
+  //https://codepen.io/brianjmacdonald/pen/VamEWG?q=ajax%20json&limit=all&order=popularity&depth=everything&show_forks=false
+  var apiResult;
+	$.ajax({
+		type: "GET",
+		dataType: "json",
+		url: "https://raw.githubusercontent.com/saamerm/Unearthed-Hackathon-Denver/master/values.json",
+		success: function(json) {
+			var inputOptions = '';
+      apiResult=json.value;
+console.log(apiResult-2);
+       // alert('Success');
+      //return json.value;
+		}, 
+		error: function(data) {
+			alert('Error loading data, please check url.');
+		}
+	});
+  console.log("ye");
+  return apiResult;
+}
